5472bb205691019b4bc5953befe9c313be03cd2c
Fati <fati.marmullaku@gmail.com>
2019-06-27 10:58:10 +0200

    Renamed Program

--- src/main/java/com/ucx/domainexercise/Application2.java
+++ src/main/java/com/ucx/domainexercise/Application2.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Application {
+public class Application2 {
     public static void main(String[] args) {
         StorageService<Costumer, Integer> ssi = new StorageServiceImpl<>();
 
